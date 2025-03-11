4c89cf692d2a510ffedb3fda46287e2c2be3de4b
Aron Pavel <ext.aron.pavel@sats.no>
2025-03-12 00:27:35 +0100

    Fixed plural forms for date segments

--- static/scripts/credential.js
+++ static/scripts/credential.js
@@ -5,7 +5,7 @@
     event.preventDefault();
     var pwd_confirm = document.getElementById('pwd_confirm').value;
 
-    if (event.target.password.value != pwd_confirm) {
+    if (event.target.password.value !== pwd_confirm) {
       alert('Passwords do not match!');
       return false;
     }
--- static/scripts/status.js
+++ static/scripts/status.js
@@ -3,19 +3,28 @@
     .querySelector("meta[name='boot_time']")
     .getAttribute('content');
 
+  var getUnit = function(value, unitSingular) {
+    return value > 1 ? unitSingular.concat('s') : unitSingular;
+  };
+
   setInterval(function() {
     var now = new Date();
     var elapsed = new Date(now - parseInt(bootMeta) * 1000);
 
+    var readableDate = elapsed.getUTCDate() - 1;
+    var readableHours = elapsed.getUTCHours();
+    var readableMinutes = elapsed.getUTCMinutes();
+    var readableSeconds = elapsed.getUTCSeconds();
+
     var readable = [
-      (elapsed.getUTCDate() - 1),
-      'days',
-      elapsed.getUTCHours(),
-      'hours',
-      elapsed.getUTCMinutes(),
-      'minutes',
-      elapsed.getUTCSeconds(),
-      'seconds'
+      readableDate,
+      getUnit(readableDate, 'day'),
+      readableHours,
+      getUnit(readableHours, 'hour'),
+      readableMinutes,
+      getUnit(readableMinutes, 'minute'),
+      readableSeconds,
+      getUnit(readableSeconds, 'second'),
     ].join(' ');
 
     document.getElementById('uptime').innerHTML = readable;
